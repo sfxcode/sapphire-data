d23b2ea0c16a62405c1b365a6496cb511661dd25
tom <tom@sfxcode.com>
2020-11-10 10:58:56 +0100

    jasperreports

--- src/main/scala/com/sfxcode/sapphire/data/report/PdfExporter.scala
+++ src/main/scala/com/sfxcode/sapphire/data/report/PdfExporter.scala
@@ -7,25 +7,24 @@ import net.sf.jasperreports.engine._
 import net.sf.jasperreports.engine.`export`.JRPdfExporter
 
 case class PdfExporter(jasperUrl: URL, reportContext: JasperReportsContext = DefaultJasperReportsContext.getInstance())
-    extends AbstractExporter(jasperUrl) {
+  extends AbstractExporter(jasperUrl) {
   private val exporter = new JRPdfExporter(reportContext)
 
   /**
-    * Report Exporter
-    * @param exportFile
-    * @param parameter
-    * @param dataSource
-    * @param exporterConfiguration
-    * @param reportConfiguration
-    * @return ReportExportResult
-    */
+   * Report Exporter
+   * @param exportFile
+   * @param parameter
+   * @param dataSource
+   * @param exporterConfiguration
+   * @param reportConfiguration
+   * @return ReportExportResult
+   */
   def exportReport(
-      exportFile: File,
-      parameter: Map[String, AnyRef] = Map(),
-      dataSource: JRDataSource = new JREmptyDataSource(),
-      exporterConfiguration: PdfExporterConfiguration = new SimplePdfExporterConfiguration(),
-      reportConfiguration: SimplePdfReportConfiguration = new SimplePdfReportConfiguration()
-  ): ReportExportResult = {
+    exportFile: File,
+    parameter: Map[String, AnyRef] = Map(),
+    dataSource: JRDataSource = new JREmptyDataSource(),
+    exporterConfiguration: PdfExporterConfiguration = new SimplePdfExporterConfiguration(),
+    reportConfiguration: SimplePdfReportConfiguration = new SimplePdfReportConfiguration()): ReportExportResult = {
 
     val startTime = System.currentTimeMillis()
     try {
@@ -35,8 +34,7 @@ case class PdfExporter(jasperUrl: URL, reportContext: JasperReportsContext = Def
       exporter.setConfiguration(reportConfiguration)
       exporter.exportReport()
       ReportExportResult(completed = true, System.currentTimeMillis() - startTime, exportFile, jasperUrl)
-    }
-    catch {
+    } catch {
       case e: Exception =>
         logger.debug(e.getMessage, e)
         ReportExportResult(completed = false, System.currentTimeMillis() - startTime, exportFile, jasperUrl, Some(e))
