6699b4b554e848efbc705ebdd213c16609b92322
Thomas Lamers <thomas.lamers@iwelt.de>
2021-03-13 23:37:25 +0100

    prevent exception if name not in field map

--- src/main/scala/com/sfxcode/sapphire/data/reflect/ReflectionTools.scala
+++ src/main/scala/com/sfxcode/sapphire/data/reflect/ReflectionTools.scala
@@ -12,24 +12,31 @@ object ReflectionTools extends LazyLogging {
   }
 
   def setFieldValue(target: Any, name: String, value: Any): Unit = {
-    val fieldMeta = FieldMetaRegistry.fieldMeta(target.asInstanceOf[AnyRef], name)
-    val field     = fieldMeta.field.get
+    val fieldMeta   = FieldMetaRegistry.fieldMeta(target.asInstanceOf[AnyRef], name)
+    val fieldOption = fieldMeta.field
 
-    try {
-      field.setAccessible(true)
+    if (fieldOption.isDefined) {
+      val field = fieldOption.get
+      try {
+        field.setAccessible(true)
 
-      if (fieldMeta.isOption)
-        if (value == null)
-          field.set(target, None)
-        else if (value.isInstanceOf[Option[_]])
-          field.set(target, value)
+        if (fieldMeta.isOption)
+          if (value == null)
+            field.set(target, None)
+          else if (value.isInstanceOf[Option[_]])
+            field.set(target, value)
+          else
+            field.set(target, Some(value))
         else
-          field.set(target, Some(value))
-      else
-        field.set(target, value)
+          field.set(target, value)
+      }
+      catch {
+        case exc: Exception =>
+          logger.debug("can not update %s for field %s - %s".format(value, name, exc.getMessage))
+      }
     }
-    catch {
-      case _: Exception => logger.debug("can not update %s for field %s".format(value, name))
+    else {
+      logger.debug("can not update %s for field %s - Not Found".format(value, name))
     }
 
   }
